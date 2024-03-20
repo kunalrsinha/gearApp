dcbb215e2e347e9e3d272dc2655f8d4f911a1e7b
Kunal Sinha <ksinha@ekcs.co>
2024-03-20 15:01:34 +0530

    Validation changes

---
+++ src/app/auth-services.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServicesService {
+
+  constructor() { }
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,29 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import swal from 'sweetalert2';
+import { AuthServicesService } from '../auth-services.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 
 
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+  loginForm!: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder,private authService: AuthServicesService) {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      password: ['',[Validators.required,Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&]).{8,}')]]
     });
    }
 
-
+   get f() { return this.loginForm.controls; }
    onSubmit() {
     // Handle form submission here
-    console.log(this.loginForm.value);
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      console.log("no data found");
+      return;
+    }
+    let  userData= {
+      email  : this.loginForm.value.email,
+      password  : this.loginForm.value.password
+    }
+    console.log("email:"+ userData.email,"password:"+userData.password);
   }
 
   ngOnInit(): void {
-    console.log(this.loginForm?.controls?.['email']?.['errors']?.['email'],"aaaaaaaaaa")
+    console.log(this.loginForm?.controls?.['password']?.errors?.['minLength'],"aaaaaaaaaa")
+
     // console.log(this.loginForm?.controls?.['password']['touched'],"aaaaaddaaaaa")
   }
 
